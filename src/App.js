265f5eac7b68bea9f569e17786cba9ac398fb76c
Jagjot Singh <dev.flenex@gmail.com>
2023-12-07 22:04:19 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Section from './components/Section'
+import Product from './components/Product'
+
+// ABIs
+import Dappazon from './abis/Dappazon.json'
+
+// Config
+import config from './config.json'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h2>Welcome to Dappazon</h2>
+
     </div>
   );
 }
